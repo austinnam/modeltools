cb65d3d4b18c9b7019a8b1e492d23920e95a3d35
Austin Nam <austin.nam@gmail.com>
2018-07-25 16:51:32 -0400

    Added exception handling

--- R/convert.R
+++ R/convert.R
@@ -2,78 +2,74 @@
 #' @description Converts a vector of probabilities to a vector of rates. 
 #' @param p A vector of probabilities.
 #' @param t The time frame of the probability. 
-#' @details If any probabilities are less than 0 or greater than 1, the function returns \emph{NULL}. 
-#' @return A vector of rates in the unit of time \emph{t}.
+#' @return A vector of rates in the unit of time \emph{t}. If any probabilities are less than 0 or greater than 1, the function returns \emph{NULL}. 
 #' @examples 
 #' # For a cumulative probability of 50% over 2 years
 #' ProbToRate(0.5,2) # returns an annual rate of 0.35
 #' ProbToRate(0.5,24) # returns a monthly rate of 0.02 
 ProbToRate <- function(p, t=1){
-  # Exception handling for probabilities outside (0,1)
-  if(any(p<0)){
-    print("ERROR (ProbToRate): Passed probability less than 0. Returning NULL.")
-    return()
-  }
-  else if(any(p>1)){
-    print("ERROR (ProbToRate): Passed probability greater than 1. Returning NULL.")
-    return()
-  }
   
-  else return(-log(1-p)/t)
+  tryCatch(
+    
+    return(-log(1-p)/t),
+    
+    if(any(p<0)) print("ERROR (ProbToRate): Passed probability less than 0. Returning NULL."), # exception handling for prob<0
+    if(any(p>1)) print("ERROR (ProbToRate): Passed probability greater than 1. Returning NULL."), # exception handling for prob>1
+    
+    return() # return NULL of exception occurs
+  )
 }
 
 
 #' @title Convert a rate to a probability
 #' @description Converts a vector of rates to a vector of probabilities. 
-#' @details If any rates are less than 0, the function returns \emph{NULL}.
 #' @param r A vector of rates.
 #' @param t The time frame of the desired probability in the units of the input rate.
-#' @return A vector of probabilities in the unit of time \emph{t}.
+#' @return A vector of probabilities in the unit of time \emph{t}. If any rates are less than 0, the function returns \emph{NULL}.
 #' @examples 
 #' # If event rate over over 2 years is 50%
 #' RateToProb(0.5, 1/2) # returns an annual probability of 0.22
 #' RateToProb(0.5, 1/24) # returns a monthly probability of 0.02
 RateToProb <- function(r, t=1){
-  # Exception handling for rates < 0.
-  if(any(r<0)){
-    print("ERROR (RateToProb): Passed a rate less than 0. Returning NULL.")
-    return()
-  }
   
-  else return(1-exp(-r*t))
+  tryCatch(
+    
+    return(1-exp(-r*t)),
+    
+    if(any(r<0)) print("ERROR (RateToProb): Passed a rate less than 0. Returning NULL."), # exception handling for rates less than 0
+    
+    return() # return NULL if exception occurs
+  )
 }
 
 #' @title Convert probability to odds
 #' @description Converts a vector of probabilities to odds. 
-#' @details If any probabilities are less than 0 or greater than 1, the function returns a \emph{NULL}.
 #' @param p A vector of probabilities.
-#' @return A vector of odds.
+#' @return A vector of odds. If any probabilities are less than 0 or greater than 1, the function returns a \emph{NULL}.
 ProbToOdds <- function(p){
-  # Exception handling for probabilities outside (0,1)
-  if(any(p<0)){
-    print("ERROR (ProbToOdds): Passsed a probability less than 0. Returning NULL.")
-    return()
-  }
-  else if(any(p>1)){
-    print("ERROR (ProbToOdds): Passed a probability greater than 1. Returning NULL.")
-    return()
-  }
   
-  else return(p/(1-p))
+  tryCatch(
+    return(p/(1-p)),
+    
+    if(any(p<0)) print("ERROR (ProbToOdds): Passsed a probability less than 0. Returning NULL."), # exception handling if prob < 0
+    if(any(p>1)) print("ERROR (ProbToOdds): Passed a probability greater than 1. Returning NULL."), # exception if prob > 1
+    
+    return() # return NULL if exception occurs
+  )
+  
 }
 
 #' @title Convert odds to probability
 #' @description Converts a vector of odds to probabilities. 
-#' @details If any odds are less than 0, the function returns \emph{NULL}.
 #' @param o A vector of odds.
-#' @return A vector of probabilities.
+#' @return A vector of probabilities. If any odds are less than 0, the function returns \emph{NULL}.
 OddsToProb <- function(o){
-  # Exception handling for any odds < 0.
-  if(any(o<0)){
-    print("ERROR (OddsToProb): Passed an odds less than 0. Returning NULL.")
-    return()
-  }
   
-  else return(o/(1+o))
-  
+  tryCatch(
+    return(o/(1+o)),
+    
+    if(any(o<0)) print("ERROR (OddsToProb): Passed an odds less than 0. Returning NULL."), # exception if odds < 0
+    return() # return NULL if exception occurs
+  )
+
 }
--- R/payoff.R
+++ R/payoff.R
@@ -3,7 +3,16 @@
 #' @param disc_rate The discount rate at time \emph{0}.
 #' @param t A vector of time points in the Markov chain.
 #' @details \emph{disc_rate} and \emph{t} must be in the same time units (i.e. discount rate = 1.5% per annum, t is in years).
-#' @return A vector of discount rates for each time point in a Markov chain.
+#' @return A vector of discount rates for each time point in a Markov chain. A \emph{NULL} value is returned if \emph{disc_rate} or any value in \emph{t} is less than 0. 
 GetDiscount <- function(disc_rate, t){
-  return(1/((1+disc_rate)^seq(0,t-1)))
+  
+  tryCatch(
+    return(1/((1+disc_rate)^seq(0,t-1))),
+    
+    if(disc_rate<0) print("ERROR (GetDiscount): Passed a discount rate less than 0. Returning NULL."), 
+    if(any(t<0)) print("ERROR (GetDiscount): Passed a time value less than 0. Returning NULL."),
+    
+    return() # return NULL if exception occurs
+  )
+  
 }
